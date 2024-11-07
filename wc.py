2ff1222d3978992a1e52d70f685db6b2f5aaf599
qdos-git <183282217+qdos-git@users.noreply.github.com>
2024-11-07 13:30:37 +0000

    Minor update to Python

--- wc.py
+++ wc.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
+##  How Python is suppose to be...
+
 import sys
 import subprocess
 
-subprocess.run(["go", "run", "wc", sys.argv[1]])
+subprocess.run(["go", "run", "wc.go", sys.argv[1]])
 
 ##  Note: above untested.
+
+##  How Python really is...
+
+exec(go run wc.go)
