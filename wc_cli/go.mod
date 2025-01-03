ab9d5783c986db87381c15079a3558d09ccafa6a
qdos-git <183282217+qdos-git@users.noreply.github.com>
2025-01-03 17:26:14 +0000

    Still reorganising code into modules, for later testing.

---
+++ wc_cli/go.mod
@@ -0,0 +1,7 @@
+module wc_cli
+
+go 1.23.4
+
+replace wc_lib => ../wc_lib
+
+require wc_lib v0.0.0-00010101000000-000000000000
--- wc_lib/wc_lib.go
+++ wc_lib/wc_lib.go
@@ -28,9 +28,7 @@ func wc_core(args []string) {
 
 	data := load_data(filename)
 
-	chars, words, lines, bytec := count_chars(data), count_words(data), count_lines(data), len(data)
-
-	output_data(chars, words, lines, bytec, args, filename)
+	return count_chars(data), count_words(data), count_lines(data), len(data)
 
 }
 
@@ -153,3 +151,44 @@ func count_lines(data []byte) int {
 	return newlines_count
 
 }
+
+func output_data(chars int, words int, lines int, bytec int, args []bool, filename string) {
+
+	if !args[0] && !args[1] && !args[2] && !args[3] && !args[4] {
+
+		fmt.Println(words, lines, chars, filename)
+
+	} else {
+
+		if args[0] {
+
+			fmt.Printf("%d ", words)
+
+		}
+
+		if args[1] {
+
+			fmt.Printf("%d ", lines)
+
+		}
+
+		if args[2] {
+
+			fmt.Printf("%d ", bytec)
+
+		}
+
+		if args[3] {
+
+			fmt.Printf("%d ", chars)
+
+		}
+
+		fmt.Println(filename)
+
+	}
+
+}
+
+
+
